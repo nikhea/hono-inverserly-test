b493abc408f99429f964b3dd74249bd87045cd1f
nikhea <imonikheaugbodaga@gmail.com>
2026-06-02 03:10:21 +0100

    added the core packages

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import {
+  type HonoBindings,
+  type HonoVariables,
+  MastraServer,
+} from "@mastra/hono";
+import { mastra } from "./mastra/index.js";
 
-const app = new Hono()
+const app = new Hono<{ Bindings: HonoBindings; Variables: HonoVariables }>();
+const server = new MastraServer({ app, mastra });
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+await server.init();
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
